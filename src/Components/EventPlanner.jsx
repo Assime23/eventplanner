17529990f1aa4cbab6b97b42398e86a4aeb13ab4
assime23 <assimekere@gmail.com>
2025-01-24 14:16:10 -0500

    V2

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -7,6 +7,30 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+              <div className="event-planner-container">
+            <header>
+                <h1>Bienvenue dans Event Planner</h1>
+            </header>
+            <section className="description">
+                 <p>
+                    Planifiez et organisez vos événements sans effort avec Event Planner. Des anniversaires aux réunions d'entreprise, nous avons tout ce qu'il vous faut.
+                 </p>
+                 <button className="get-started-button">Commencer</button>
+           
+            </section>
+            <section className="events_categories">
+
+            </section>
+            <section className="features">
+
+            </section>
+            <section className="testimonials">
+
+            </section>
+            <section className="contact">
+
+            </section>
+        </div>
         </div>
     );
 };
